183c65c742d3c4a96983dac6f7969a5f57ed00dc
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-16 11:16:12 -0500

    Create tutorial part 1

---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+
+const TodoItem = (props) => <li>{props.todo.title}</li>;
+
+export default TodoItem;
